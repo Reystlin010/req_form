06db1740b5e81af2545992f4ba0c45d3a2a2dd53
Georgy Pestovsky <workbench010@gmail.com>
2023-04-19 20:31:33 +0200

    cookie variant

--- testfield.py
+++ testfield.py
@@ -1,5 +1,6 @@
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from typing import Annotated
 from standarting_phone import unify_phone
 from fastapi.responses import Response
 
@@ -7,15 +8,21 @@ from fastapi.responses import Response
 app = FastAPI()
 
 
-@app.get("/unify_phone_from_query")
-def return_the_phone(phone = str):
-    unified_phone = unify_phone(phone) 
-    response = Response(
-        f"{unified_phone}",
-        media_type = "text/html"
-    )
-    return response
+@app.get("/unify_phone_from_cookies")
+async def return_the_phone(request: Request):
+    phone_data = request.headers.get('Cookie')
+    if phone_data:
+        unified_phone = unify_phone(phone_data.split("=")[1]) 
+        response = Response(
+            f"{unified_phone}",
+            media_type = "text/html"
+        )
+        return response
+    else:
+        return Response("not ok")
 
     
 if __name__ == "__main__":
     uvicorn.run(app, host="195.135.253.40", port=8000)
+
+# 195.135.253.40
