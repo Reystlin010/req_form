b0e3938943e61d721c2d532af9239d0f200d1e42
Georgy Pestovsky <workbench010@gmail.com>
2023-04-19 19:44:41 +0200

    query

--- testfield.py
+++ testfield.py
@@ -1,6 +1,5 @@
-import json
 import uvicorn
-from fastapi import Request, FastAPI, Form
+from fastapi import FastAPI
 from standarting_phone import unify_phone
 from fastapi.responses import Response
 
@@ -8,26 +7,15 @@ from fastapi.responses import Response
 app = FastAPI()
 
 
-@app.post("/unify_phone_from_form")
-def return_the_phone(json_with_phone: str = Form(...)):
-    content_type = json_with_phone.headers.get('Content-Type')
+@app.get("/unify_phone_from_query")
+def return_the_phone(phone = str):
+    unified_phone = unify_phone(phone) 
+    response = Response(
+        f"{unified_phone}",
+        media_type = "text/html"
+    )
+    return response
 
-    if content_type is None:
-        return 'No content-type provided'
-    elif content_type == "multipart/form-data":
-        try:
-            data = 
-            dict_with_phone = json.dumps(data)
-            phone = unify_phone(json.loads(dict_with_phone).get("phone"))
-            response = Response(
-                f"{phone}",
-                media_type = "text/html"
-            )
-            return response
-        except ValueError:
-            return "Invalid JSON data"
-    else:
-        return "Content-Type not supported"
     
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=8000)
